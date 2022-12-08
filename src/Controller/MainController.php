6cf159085d2d9c6c6616040e9947f06fde42b715
Zakari Boureghda <zakari.boureghda@gmail.com>
2022-12-08 09:54:42 +0100

    Controller and View creation

---
+++ src/Controller/MainController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CatRepository;
+use App\Repository\HumanRepository;
+
+#[Route('/', name: 'app_')]
+class MainController extends AbstractController
+{
+    #[Route('index', name: 'index')]
+    public function index(CatRepository $catRepository, HumanRepository $humanRepository): Response
+    {
+        $cats = $catRepository->findAll();
+        $humans = $humanRepository->findAll();
+
+        return $this->render('home/index.html.twig', [
+            'cats' => $cats,
+            'humans' => $humans
+        ]);
+    }
+}
